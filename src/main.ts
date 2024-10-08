04e2f9a7a5dea66aef776df3ecb5383f592c0731
lharnist22 <167343877+lharnist22@users.noreply.github.com>
2024-10-08 07:38:43 +0000

    Step 8 Complete

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,7 @@ dupgrade3c.style.left = "50%";
 dupgrade3c.style.transform = "translate(-50%, 0)";
 document.body.appendChild(dupgrade3c); // Append counter display
 
-button.textContent = "🍪 Click"; //Emoji next to button
+button.textContent = "❚█══█❚ Rep!"; //Emoji next to button
 button.style.position = "absolute";
 button.style.top = "40%"; // Used to place button properly
 button.style.left = "50%"; // Used to place button properly
@@ -55,7 +55,7 @@ document.body.appendChild(button); // Appending button
 button.addEventListener("click", () => {
   counter += 1;
   console.log(counter);
-  counterDisplay.textContent = `Counter: ${counter}`; // Updating counter to appear properly
+  counterDisplay.textContent = `Reps: ${counter}`; // Updating counter to appear properly
 });
 
 const header = document.createElement("h1");
@@ -72,7 +72,7 @@ function increaseCounter(currTime: number) {
   //Increasing counter
   counter += growthRate * (elapsed / 1000);
 
-  counterDisplay.textContent = `Counter: ${counter}`; // Updating counter to appear properly
+  counterDisplay.textContent = `Reps: ${counter}`; // Updating counter to appear properly
 
   //Calling function recursively to continue forever
   requestAnimationFrame(increaseCounter);
@@ -86,7 +86,7 @@ function increaseCounter(currTime: number) {
 requestAnimationFrame(increaseCounter);
 
 const upgrade1 = document.createElement("button") as HTMLButtonElement;
-upgrade1.textContent = "Buy Growth Upgrade 1";
+upgrade1.textContent = "Preworkout";
 upgrade1.style.position = "absolute";
 upgrade1.style.top = "70%";
 upgrade1.style.left = "30%";
@@ -100,13 +100,13 @@ upgrade1.addEventListener("click", () => {
     counter -= 10;
     growthRate += 1;
     upgrade1c += 1;
-    counterDisplay.textContent = `Counter: ${counter}`; // Updating counter to appear properly
-    dupgrade1c.textContent = `Num of upgrade 1: ${upgrade1c}`; // Updating num of upgrades
+    counterDisplay.textContent = `Reps: ${counter}`; // Updating counter to appear properly
+    dupgrade1c.textContent = `Preworkout: ${upgrade1c}`; // Updating num of upgrades
   }
 });
 
 const upgrade2 = document.createElement("button") as HTMLButtonElement;
-upgrade2.textContent = "Buy Growth Upgrade 2";
+upgrade2.textContent = "Creatine";
 upgrade2.style.position = "absolute";
 upgrade2.style.top = "70%";
 upgrade2.style.left = "50%";
@@ -119,13 +119,13 @@ upgrade2.addEventListener("click", () => {
     counter -= 100;
     growthRate += 2;
     upgrade2c += 1;
-    counterDisplay.textContent = `Counter: ${counter}`; // Updating counter to appear properly
-    dupgrade2c.textContent = `Num of upgrade 1: ${upgrade2c}`; // Updating num of upgrades
+    counterDisplay.textContent = `Reps: ${counter}`; // Updating counter to appear properly
+    dupgrade2c.textContent = `Creatine: ${upgrade2c}`; // Updating num of upgrades
   }
 });
 
 const upgrade3 = document.createElement("button") as HTMLButtonElement;
-upgrade3.textContent = "Buy Growth Upgrade 3";
+upgrade3.textContent = "Tren";
 upgrade3.style.position = "absolute";
 upgrade3.style.top = "70%";
 upgrade3.style.left = "70%";
@@ -138,7 +138,7 @@ upgrade3.addEventListener("click", () => {
     counter -= 1000;
     growthRate += 50;
     upgrade3c += 1;
-    counterDisplay.textContent = `Counter: ${counter}`; // Updating counter to appear properly
-    dupgrade3c.textContent = `Num of upgrade 1: ${upgrade3c}`; // Updating num of upgrades
+    counterDisplay.textContent = `Reps: ${counter}`; // Updating counter to appear properly
+    dupgrade3c.textContent = `Tren: ${upgrade3c}`; // Updating num of upgrades
   }
 });
