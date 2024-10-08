c3966229e6ad71c82dbe36281f0a54158422d509
lharnist22 <167343877+lharnist22@users.noreply.github.com>
2024-10-08 05:53:53 +0000

    Finished Step 1
    Added button, emoji, comments, and cenetered button

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Luc's Game!";
 document.title = gameName;
 
-const button = document.createElement('Button');
-button.textContent = '🍪 Click'; //Emoji next to button
-button.style.position = 'absolute'
-button.style.top = '40%'; // Used to place button properly
-button.style.left = '50%'; // Used to place button properly
-button.style.transform = 'translate(-50%, -50%)'; // Used to place button properly
+const button = document.createElement("Button");
+button.textContent = "🍪 Click"; //Emoji next to button
+button.style.position = "absolute";
+button.style.top = "40%"; // Used to place button properly
+button.style.left = "50%"; // Used to place button properly
+button.style.transform = "translate(-50%, -50%)"; // Used to place button properly
 document.body.appendChild(button); // Appending button
 
 const header = document.createElement("h1");
