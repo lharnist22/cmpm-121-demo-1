f6d6b2710797931ba793f64eff81735dccfd7833
lharnist22 <167343877+lharnist22@users.noreply.github.com>
2024-10-08 06:55:46 +0000

    Step 5 Complete

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ const gameName = "Luc's Game!";
 document.title = gameName;
 
 const button = document.createElement("Button");
+
 let counter: number = 0;
+let growthRate = 0;
 
 //displaying counter
 const counterDisplay = document.createElement("p");
@@ -22,6 +24,7 @@ button.style.top = "40%"; // Used to place button properly
 button.style.left = "50%"; // Used to place button properly
 button.style.transform = "translate(-50%, -50%)"; // Used to place button properly
 document.body.appendChild(button); // Appending button
+
 button.addEventListener("click", () => {
   counter += 1;
   console.log(counter);
@@ -40,14 +43,33 @@ function increaseCounter(currTime: number) {
   start = currTime;
 
   //Increasing counter
-  counter += elapsed / 1000;
+  counter += growthRate * (elapsed / 1000);
 
   counterDisplay.textContent = `Counter: ${counter}`; // Updating counter to appear properly
 
   //Calling function recursively to continue forever
   requestAnimationFrame(increaseCounter);
   //console.log(elapsed);
+  upgrade.disabled = counter < 10;
+  
 }
 
 //Starting time cycle
 requestAnimationFrame(increaseCounter);
+
+const upgrade = document.createElement("Button");
+upgrade.textContent = "Buy Growth Upgrade";
+upgrade.style.position = "absolute";
+upgrade.style.top = "70%";
+upgrade.style.left = "50%";
+upgrade.style.transform = "translate(-50%, 0)";
+upgrade.disabled = true; // Initially disabled
+document.body.appendChild(upgrade);
+
+upgrade.addEventListener("click", () => {
+    if (counter >= 10){
+        counter -= 10;
+        growthRate += 1;
+        counterDisplay.textContent = `Counter: ${counter}`; // Updating counter to appear properly
+    }
+})
