27736ccae0d9db86529d44eef4616da18eb2b0a8
lharnist22 <167343877+lharnist22@users.noreply.github.com>
2024-10-08 06:08:09 +0000

    Step 2 Complete
    Added counter that properly displays # of times clicked

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,6 @@ counterDisplay.style.left = "50%";
 counterDisplay.style.transform = "translate(-50%, 0)";
 document.body.appendChild(counterDisplay); // Append counter display
 
-
 button.textContent = "🍪 Click"; //Emoji next to button
 button.style.position = "absolute";
 button.style.top = "40%"; // Used to place button properly
@@ -24,14 +23,11 @@ button.style.left = "50%"; // Used to place button properly
 button.style.transform = "translate(-50%, -50%)"; // Used to place button properly
 document.body.appendChild(button); // Appending button
 button.addEventListener("click", () => {
-    counter += 1;
-    console.log(counter);
-    counterDisplay.textContent = `Counter: ${counter}`; //updating counter to appear properly 
+  counter += 1;
+  console.log(counter);
+  counterDisplay.textContent = `Counter: ${counter}`; //updating counter to appear properly
 });
-
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
-
-
