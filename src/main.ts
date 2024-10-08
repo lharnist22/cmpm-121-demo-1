3dcfddf9ca74ec2a53b581a9a1542084a4ffbd77
lharnist22 <167343877+lharnist22@users.noreply.github.com>
2024-10-08 05:53:41 +0000

    Step 1 Complete
    Added button and some comments, also I centered button and put cookie emoji

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Luc's Game!";
 document.title = gameName;
 
+const button = document.createElement('Button');
+button.textContent = '🍪 Click'; //Emoji next to button
+button.style.position = 'absolute'
+button.style.top = '40%'; // Used to place button properly
+button.style.left = '50%'; // Used to place button properly
+button.style.transform = 'translate(-50%, -50%)'; // Used to place button properly
+document.body.appendChild(button); // Appending button
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
