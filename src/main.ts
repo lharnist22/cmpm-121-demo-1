280f77d5f546a3440b1782274cbd565824fce4fe
lharnist22 <167343877+lharnist22@users.noreply.github.com>
2024-10-08 06:08:00 +0000

    Step 2 Complete
    Added an increasing counter under the button

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,32 @@ const gameName = "Luc's Game!";
 document.title = gameName;
 
 const button = document.createElement("Button");
+let counter = 0;
+
+//displaying counter
+const counterDisplay = document.createElement("p");
+counterDisplay.style.position = "absolute";
+counterDisplay.style.top = "60%"; // Position under the button
+counterDisplay.style.left = "50%";
+counterDisplay.style.transform = "translate(-50%, 0)";
+document.body.appendChild(counterDisplay); // Append counter display
+
+
 button.textContent = "🍪 Click"; //Emoji next to button
 button.style.position = "absolute";
 button.style.top = "40%"; // Used to place button properly
 button.style.left = "50%"; // Used to place button properly
 button.style.transform = "translate(-50%, -50%)"; // Used to place button properly
 document.body.appendChild(button); // Appending button
+button.addEventListener("click", () => {
+    counter += 1;
+    console.log(counter);
+    counterDisplay.textContent = `Counter: ${counter}`; //updating counter to appear properly 
+});
+
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+
